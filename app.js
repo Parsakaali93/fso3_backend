a7df070c2f12e8594a508f7dddf8edd4a3b0b4ff
Parsakaali <maukissamau@gmail.com>
2023-12-02 12:59:55 +0200

    fixed things

--- app.js
+++ app.js
@@ -18,16 +18,7 @@ const notesRouter = require('./controllers/notes')
 // Näin muuttuja Note saa arvokseen saman olion, jonka moduuli määrittelee.
 const Note = require('./models/note')
 
-/*Jotta pääsisimme POST-pyynnön mukana lähetettyyn dataan helposti 
-käsiksi, tarvitsemme Expressin tarjoaman json-parserin apua. */
-app.use(express.json())
-app.use(cors())
-app.use('/api/notes', notesRouter)
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-/* Jotta saamme Expressin näyttämään staattista sisältöä eli sivun index.html
- ja sen lataaman JavaScriptin ym. tarvitsemme Expressiin sisäänrakennettua middlewarea static. */
-app.use(express.static('dist'))
+
 
 let notes = [
   {
@@ -63,6 +54,18 @@ mongoose.connect(config.MONGODB_URI)
     console.log('error connecting to MongoDB:', error.message)
   })
 
+  /*Jotta pääsisimme POST-pyynnön mukana lähetettyyn dataan helposti 
+käsiksi, tarvitsemme Expressin tarjoaman json-parserin apua. */
+app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
+app.use(middleware.requestLogger)
+
+app.use('/api/notes', notesRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+/* Jotta saamme Expressin näyttämään staattista sisältöä eli sivun index.html
+ ja sen lataaman JavaScriptin ym. tarvitsemme Expressiin sisäänrakennettua middlewarea static. */
 /*
 const PORT = process.env.PORT
 app.listen(PORT, () => {
