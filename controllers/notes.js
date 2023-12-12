6acdf71da8c5707c172943712d3132ccccbdf5e2
Parsakaali <maukissamau@gmail.com>
2023-12-12 21:48:25 +0200

    notes for logged in users only

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,6 +2,17 @@ const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
 
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {  
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+        }  
+
+  return null
+}
 // Kaikkien muistiinpanojen nouto
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({})
@@ -30,7 +41,18 @@ notesRouter.get('/:id', async (request, response) => {
 // Uuden muistiinpanon lisäys
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
+
+  /* Apufunktio getTokenFrom eristää tokenin headerista authorization.
+   Tokenin oikeellisuus varmistetaan metodilla jwt.verify. Metodi myös 
+   dekoodaa tokenin, eli palauttaa olion, jonka perusteella token on laadittu: */
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })  
+  }  
+  
+  const user = await User.findById(decodedToken.id)
+
+  //const user = await User.findById(body.userId)
 
   console.log(request.body)
   console.log(body.content)
